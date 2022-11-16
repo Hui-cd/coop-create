44d02e97cf97145405c5e97f0485aeae96422ecd
Hui-cd <gyh1429107530@gmail.com>
2022-11-16 04:16:56 +0000

    Initial commit

---
+++ src/test/java/coopcreate/CoopCreateApplicationTests.java
@@ -0,0 +1,13 @@
+package coopcreate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoopCreateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
